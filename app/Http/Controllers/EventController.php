884d5f295f97719b0585f9ec72ccc0c17c07129c
Clayton <cdston@gmail.com>
2022-04-10 11:09:29 -0300

    exibindo eventos do usuario

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -82,7 +82,12 @@ class EventController extends Controller
 
         $events = $user->events;
 
-        return view('events.dashboard',['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view('events.dashboard',[
+            'events' => $events,
+            'eventsasparticipant' => $eventsAsParticipant
+        ]);
     }
 
     public function destroy($id)
@@ -94,7 +99,13 @@ class EventController extends Controller
 
     public function edit($id)
     {
+
+        $user = auth()->user();
         $event = Event::findOrFail($id);
+
+        if($user->id != $event->user->id){
+            return redirect('/dashboard');
+        }
 
         return view('events.edit',['event' => $event]);
     }
